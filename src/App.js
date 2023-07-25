f6420f96cb32e580a000db60026bf650b3458af7
Almas K S <mailmealmasks@gmail.com>
2023-07-25 19:19:34 +0530

    weather app created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios';
+import './app.css';
 
 function App() {
+  const [data,setData]=useState({})
+  const [location,setLocation]=useState('')
+  const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c23872bbf96bdfaa6081e44656fb0d0e`
+  
+  const searchLocation=(e)=>{
+    if(e.key==='Enter'){
+      axios.get(url)
+      .then(res=>{setData(res.data)
+        console.log(res.data)
+      }
+      )
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+          <input placeholder='Enter Location' type="text" value={location} onChange={e=>setLocation(e.target.value)}
+          onKeyPress={searchLocation}/>
+            <p>{data.name}</p>
+          </div>
+          <div className='temp'>
+            
+            <h1>{data.main}</h1>
+          </div>
+          <div className='description'>
+            <p>clouds</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'>
+            <p>65 F</p>
+            <p>Feels like</p>
+          </div>
+          <div className='humidity'>
+            <p>20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className='wind'>
+            <p >12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+
+      </div>
+      
     </div>
   );
 }
